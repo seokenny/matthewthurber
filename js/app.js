88fef2d7c28e5ae4a12998d5fe26b83b329d9027
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2019-04-26 20:21:59 -0400

    For Surface book

--- js/app.js
+++ js/app.js
@@ -12,18 +12,83 @@ document.querySelector(".mobile_nav_menu").addEventListener("click",function(){
 
 //HOMEPAGE
 document.querySelector(".m_home").addEventListener("click",function(){
-    document.querySelector(".page_contain").classList.add("fadeOut");
-    document.querySelector(".page_contain").classList.add("hidden");
-    document.querySelector(".banner_name").classList.add("fadeIn");
-    document.querySelector(".banner_name").classList.remove("fadeOut");
-    document.querySelector(".banner_name").classList.remove("hidden");
+    aboutContainOut();
+    projectsContainOut();
+    potlatchContainOut();
+    bannerNameIn();
 });
 
 //ABOUT ME
 document.querySelector(".m_about").addEventListener("click",function(){
+    bannerNameOut();
+    projectsContainOut();
+    potlatchContainOut();
+    aboutContainIn();
+});
+
+//PROJECTS
+document.querySelector(".m_projects").addEventListener("click",function(){
+    bannerNameOut();
+    aboutContainOut();
+    potlatchContainOut();
+    projectsContainIn();
+});
+
+//POTLATCH
+document.querySelector(".m_potlatch").addEventListener("click",function(){
+    bannerNameOut();
+    aboutContainOut();
+    projectsContainOut();
+    potlatchContainIn();
+});
+
+
+//---------------------------------------------------------------
+//FUNCTIONS
+//aboutContain functions
+function aboutContainOut(){
+    document.querySelector(".about_contain").classList.add("fadeOut");
+    document.querySelector(".about_contain").classList.add("hidden");
+}
+
+function aboutContainIn(){
+    document.querySelector(".about_contain").classList.add("fadeIn");
+    document.querySelector(".about_contain").classList.remove("fadeOut");
+    document.querySelector(".about_contain").classList.remove("hidden");
+}
+
+//bannerName functions
+function bannerNameOut(){
     document.querySelector(".banner_name").classList.add("fadeOut");
     document.querySelector(".banner_name").classList.add("hidden");
-    document.querySelector(".page_contain").classList.add("fadeIn");
-    document.querySelector(".page_contain").classList.remove("fadeOut");
-    document.querySelector(".page_contain").classList.remove("hidden");
-});
+}
+
+function bannerNameIn(){
+    document.querySelector(".banner_name").classList.add("fadeIn");
+    document.querySelector(".banner_name").classList.remove("fadeOut");
+    document.querySelector(".banner_name").classList.remove("hidden");
+}
+
+//projectsContain functions
+function projectsContainOut(){
+    document.querySelector(".projects_contain").classList.add("fadeOut");
+    document.querySelector(".projects_contain").classList.add("hidden");
+}
+
+function projectsContainIn(){
+    document.querySelector(".projects_contain").classList.add("fadeIn");
+    document.querySelector(".projects_contain").classList.remove("fadeOut");
+    document.querySelector(".projects_contain").classList.remove("hidden");
+}
+
+//potlatchContain functions
+function potlatchContainOut(){
+    document.querySelector(".potlatch_contain").classList.add("fadeOut");
+    document.querySelector(".potlatch_contain").classList.add("hidden");
+}
+
+function potlatchContainIn(){
+    document.querySelector(".potlatch_contain").classList.add("fadeIn");
+    document.querySelector(".potlatch_contain").classList.remove("fadeOut");
+    document.querySelector(".potlatch_contain").classList.remove("hidden");
+}
